0e3bd3ec449fc785d0003343f7c0a3f60ed66cea
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-08-29 02:40:05 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -4,7 +4,28 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
+let myModal = (document.querySelector("#modal").className = "hidden");
+let likeButtons = document.querySelectorAll(".like-glyph");
+likeButtons.forEach(function (likeButton) {
+	likeButton.addEventListener("click", function (e) {
+		mimicServerCall()
+			.then(function () {
+				if (likeButton.innerText === EMPTY_HEART) {
+					likeButton.innerText = FULL_HEART;
+					likeButton.className = "activated-heart";
+				} else {
+					likeButton.innerText = EMPTY_HEART;
+					likeButton.className = "";
+				}
+			})
+			.catch(function (error) {
+				myModal.className = "";
+				setTimeout(function () {
+					myModal.className = "hidden";
+				}, 5000);
+			});
+	});
+});
 
 
 //------------------------------------------------------------------------------
